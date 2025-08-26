a9e38d42f8633c803db535d424065a8e3c1450a0
demonlord2002 <drdoom2003p@gmail.com>
2025-08-26 14:07:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("27758016"))
+API_HASH = os.getenv("8d34cfffe27ab461eabbf0091b1a27df")
+BOT_TOKEN = os.getenv("8458120677:AAE-_r9xzg3Tg1PchwlwUmMPcBUUqmOP1MM")
 
-OWNER_IDS = list(map(int, os.getenv("OWNER_IDS").split(",")))
+OWNER_IDS = list(map(int, os.getenv("OWNER_IDS").split("7590607726")))
 
-emails = os.getenv("MEGA_EMAILS").split(",")
-passwords = os.getenv("MEGA_PASSWORDS").split(",")
+emails = os.getenv("MEGA_EMAILS").split("timepassmarkus04@gmail.com")
+passwords = os.getenv("MEGA_PASSWORDS").split("SE2-aJGLqYuek97")
 MEGA_ACCOUNTS = [{"email": e, "password": p} for e, p in zip(emails, passwords)]
 
 TMP_DOWNLOAD_PATH = "/tmp"
