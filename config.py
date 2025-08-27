65d785e30bf0be90fe1483e559a8de442b0cbf76
demonlord2002 <drdoom2003p@gmail.com>
2025-08-28 00:21:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,3 +8,12 @@ HEROKU_APP_NAME = os.environ.get('HEROKU_APP_NAME', '')
 
 # Port for Heroku
 PORT = int(os.environ.get('PORT', 8443))
+
+# Allowed file types
+ALLOWED_EXTENSIONS = ['.mp4', '.mkv', '.avi', '.mov', '.wmv', '.flv', '.webm']
+
+# Maximum file size in bytes (2GB)
+MAX_FILE_SIZE = 2 * 1024 * 1024 * 1024
+
+# Admin user ID (optional)
+ADMIN_ID = os.environ.get('ADMIN_ID', '')
