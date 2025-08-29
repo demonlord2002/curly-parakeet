47f86c46fac5bd9671b3933cfe9cf2e38955286e
demonlord2002 <drdoom2003p@gmail.com>
2025-08-29 22:22:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,19 +2,23 @@ import os
 
 class Config:
     # Telegram API
-    API_ID = int(os.getenv("API_ID", "1234567"))         # your api_id from my.telegram.org
-    API_HASH = os.getenv("API_HASH", "your_api_hash")   # your api_hash from my.telegram.org
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token") # your bot token from BotFather
+    API_ID = int(os.getenv("API_ID", "22201946"))         # your api_id from my.telegram.org
+    API_HASH = os.getenv("API_HASH", "f4e7f0de47a09671133ecafa6920ebbe")   # your api_hash from my.telegram.org
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "8458120677:AAE-_r9xzg3Tg1PchwlwUmMPcBUUqmOP1MM") # your bot token from BotFather
 
     # MongoDB
-    MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")  # MongoDB URI
+    MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://drdoom2003p:drdoom2003p@cluster0.fnhjrtn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  # MongoDB URI
 
     # Owner / Admin
-    OWNER_ID = int(os.getenv("OWNER_ID", "123456789"))   # Your Telegram ID
-    OWNER_USERNAME = os.getenv("OWNER_USERNAME", "YourUsername")  # Optional for buttons
+    OWNER_ID = int(os.getenv("OWNER_ID", "7590607726"))   # Your Telegram ID
+    OWNER_USERNAME = os.getenv("OWNER_USERNAME", "SunsetOfMe")  # Optional for buttons
 
-    # Support / Force Subscribe Channel
-    SUPPORT_CHANNEL = "@Fallen_Angels_Team" # e.g., madara_support
+    # Support Channel (for help / info only)
+    SUPPORT_CHANNEL = os.getenv("SUPPORT_CHANNEL", "Fallen_Angels_Team")
+
+    # Force Subscribe Channel (must join before using bot)
+    FORCE_SUB_CHANNEL = os.getenv("FORCE_SUB_CHANNEL", "Fallen_Angels_Team")
 
     # Allowed file extensions
     ALLOWED_EXTENSIONS = [".mp4", ".mkv"]
+    
