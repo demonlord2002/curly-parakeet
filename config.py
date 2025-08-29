d9493b1f687ee3f0da653c235a51933dcb55180c
demonlord2002 <drdoom2003p@gmail.com>
2025-08-29 17:04:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-API_ID = int(os.getenv("API_ID", "0"))
-API_HASH = os.getenv("API_HASH", "")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-BOT_USERNAME = os.getenv("BOT_USERNAME", "YourBotUsername")
-MONGO_URI = os.getenv("MONGO_URI", "")
-ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "").split(",") if x.strip().isdigit()]
+API_ID = int(os.getenv("API_ID", "22201946"))
+API_HASH = os.getenv("API_HASH", "f4e7f0de47a09671133ecafa6920ebbe")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8458120677:AAE-_r9xzg3Tg1PchwlwUmMPcBUUqmOP1MM")
+BOT_USERNAME = os.getenv("BOT_USERNAME", "FastFileLinkerBot")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://drdoom2003p:drdoom2003p@cluster0.fnhjrtn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "7590607726").split(",") if x.strip().isdigit()]
 
 MAX_FILE_SIZE_MB = int(os.getenv("MAX_FILE_SIZE_MB", "2048"))
