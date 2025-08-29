f20c0a94590744c234f9251ac110f79329843b80
demonlord2002 <drdoom2003p@gmail.com>
2025-08-29 20:07:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,14 @@
 import os
 
-API_ID = int(os.getenv("API_ID", "22201946"))
-API_HASH = os.getenv("API_HASH", "f4e7f0de47a09671133ecafa6920ebbe")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8458120677:AAE-_r9xzg3Tg1PchwlwUmMPcBUUqmOP1MM")
-BOT_USERNAME = os.getenv("BOT_USERNAME", "FastFileLinkerBot")
-MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://drdoom2003p:drdoom2003p@cluster0.fnhjrtn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "7590607726").split(",") if x.strip().isdigit()]
+class Config:
+    API_ID = int(os.getenv("API_ID", "12345"))  # get from my.telegram.org
+    API_HASH = os.getenv("API_HASH", "your_api_hash")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
 
-MAX_FILE_SIZE_MB = int(os.getenv("MAX_FILE_SIZE_MB", "2048"))
+    # Allowed extensions
+    ALLOWED_EXTENSIONS = [".mkv", ".mp4"]
+
+    # Channels & Owner
+    SUPPORT_CHANNEL = "https://t.me/Fallen_Angels_Team"
+    OWNER_USERNAME = "SunsetOfMe"
+  
